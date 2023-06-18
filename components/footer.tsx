28ba3921be76eb3e44dd519ae4d2de0ce4fba32d
fff <fff>
2023-06-18 08:12:05 +0000

    mod:footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link'
-
 import styles from '../styles/footer.module.css'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div>
-      <span>Powered by </span>
-      <Link href="https://github.com/otoyo/easy-notion-blog">
-        easy-notion-blog
-      </Link>
-    </div>
+    <div>sny</div>
+    <small>Copyright&copy; 2023 All Rights Reserved.</small>
   </footer>
 )
 
